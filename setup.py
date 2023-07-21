98b22e73e981f7516d2bcf73fe3a3c158655ee39
quentinjamet <51094990+quentinjamet@users.noreply.github.com>
2023-07-21 15:29:01 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='my_MITgcmutils',
+      version='0.0.0',
+      description='MITgcm python functions',
+      url='https://github.com/quentinjamet/my_MITgcmutils',
+      author='quentinjamet',
+      author_email='quentin.jamet@inria.fr',
+      license='MIT',
+      packages=['my_MITgcmutils'],
+      install_requires=['numpy'],
+      zip_safe=False)
